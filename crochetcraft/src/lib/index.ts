cb2a265b6990008f6ede5c2afcb2724a7c28545a
AltheFlyer <30613228+AltheFlyer@users.noreply.github.com>
2024-07-16 22:52:41 -0700

    remove index.ts

--- crochetcraft/src/lib/index.ts
+++
@@ -1,6 +0,0 @@
-// place files you want to import through the `$lib` alias in this folder.
-import './builder/bezier';
-import './builder/normalize';
-import './builder/codegen';
-
-import './geometry/cubicSpline';
