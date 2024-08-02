b0da35fbbc5fb00ab0377f859f1cbea0291956cd
Jasmine Tai <jtai@jtai.ca>
2024-08-01 20:07:57 -0700

    Add file extension to imports

--- crochet-stitcher/src/stages/3-placer.ts
+++ crochet-stitcher/src/stages/3-placer.ts
@@ -1,4 +1,4 @@
-import { LinkedStitch, Pattern, PlacedStitch } from '../types';
+import { LinkedStitch, Pattern, PlacedStitch } from '../types.js';
 
 export function place(input: Pattern<LinkedStitch>): Pattern<PlacedStitch> {
     throw 0; // TODO
