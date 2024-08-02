f4a46422305a2f946c359aa7086311e0fe13a303
Jasmine Tai <jtai@jtai.ca>
2024-08-01 20:07:57 -0700

    Add file extension to imports

--- crochet-stitcher/src/stages/2-linker.ts
+++ crochet-stitcher/src/stages/2-linker.ts
@@ -1,4 +1,4 @@
-import { LinkedStitch, ParsedStitch, Pattern } from '../types';
+import { LinkedStitch, ParsedStitch, Pattern } from '../types.js';
 
 export function link(input: Pattern<ParsedStitch>): Pattern<LinkedStitch> {
     throw 0; // TODO
