ad136806dbe76b454c62d59de4c93c9d1aeba715
Jasmine Tai <jtai@jtai.ca>
2024-09-11 17:41:05 -0400

    Remove unused code and imports

--- crochetcraft/src/lib/builder/codegen.ts
+++ crochetcraft/src/lib/builder/codegen.ts
@@ -1,23 +1,24 @@
-import { Vector3 } from "three";
+import { Vector3 } from 'three';
 
 type VectorListToCodeSettings = {
-	precision: number
-}
+    precision: number;
+};
 
 /**
- * Converts an array of points into JavaScript source code to generate that 
- * array of points. 
+ * Converts an array of points into JavaScript source code to generate that
+ * array of points.
  */
-export function vectorListToCode(points: Vector3[], settings: VectorListToCodeSettings = {
-	precision: 4,
-}) {
-	function getComponents(v: Vector3) {
-		return `${v.x.toPrecision(settings.precision)}, ${v.y.toPrecision(settings.precision)}, ${v.z.toPrecision(settings.precision)}`
-	}
+export function vectorListToCode(
+    points: Vector3[],
+    settings: VectorListToCodeSettings = {
+        precision: 4,
+    },
+) {
+    function getComponents(v: Vector3) {
+        return `${v.x.toPrecision(settings.precision)}, ${v.y.toPrecision(settings.precision)}, ${v.z.toPrecision(settings.precision)}`;
+    }
 
-	return (
-		`[
-${points.map(vec => `\tnew Vector3(${getComponents(vec)})`).join(',\n')}
-]`
-	)
+    return `[
+${points.map((vec) => `\tnew Vector3(${getComponents(vec)})`).join(',\n')}
+]`;
 }
