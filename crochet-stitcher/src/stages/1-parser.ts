74a70371a39f6ae0e4d68a262cdfb3f53023de91
Dhananjay Patki <dpatki@uwaterloo.ca>
2024-09-14 17:24:22 -0400

    Starting with basic post UI parsing

--- crochet-stitcher/src/stages/1-parser.ts
+++ crochet-stitcher/src/stages/1-parser.ts
@@ -1,5 +1,23 @@
-import { ParsedStitch, Pattern } from '../types.js';
+import { Foundation, ParsedStitch, Pattern, StitchType } from '../types.js';
 
 export function parse(input: string): Pattern<ParsedStitch> {
-    throw 0; // TODO
+    let tokens = input.split(" ")
+    // Get the foundation from somewhere??
+
+    let foundation = Foundation.SlipKnot
+    
+    //TODO: sanitize input
+    let stitches : ParsedStitch[] = []
+    for (let i = 0; i < tokens.length; i += 2) {
+        let type : StitchType = stringToStitchType(tokens[i])
+        let repeat : number = parseInt(tokens[i+1])
+        stitches.push({type, repeat})
+    }
+
+    return {foundation, stitches}
+}
+
+function stringToStitchType(input: string) {
+    // TODO: sanitize input 
+    return StitchType[input as keyof typeof StitchType]
 }
--- crochet-stitcher/test/index.spec.ts
+++ crochet-stitcher/test/index.spec.ts
@@ -1,3 +1,13 @@
+import {parse} from '../src/stages/1-parser'
+import { ParsedStitch, StitchType } from '../src/types';
+
 test('it works', () => {
     expect(2 + 2).toBe(4);
 });
+
+test('basic string parse', () => {
+    let testStr = "Chain 1, Slip 2, Single 3, Double 4, Treble 5"
+    let parsed = parse(testStr)
+    let output : ParsedStitch[] = [{type:StitchType.Chain, repeat:1}, {type:StitchType.Slip, repeat:2}, {type:StitchType.Single, repeat:3}, {type:StitchType.Double, repeat:4}, {type:StitchType.Treble, repeat:5}]
+    expect(parsed.stitches).toMatchObject(output)
+})
