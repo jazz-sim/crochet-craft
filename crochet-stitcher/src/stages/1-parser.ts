6e9d317b410a3f24247744483a3858f9eec771b7
Jasmine Tai <jtai@jtai.ca>
2024-11-06 15:57:40 -0500

    Rewrite lexer

--- crochet-stitcher/src/stages/1-parser.ts
+++ crochet-stitcher/src/stages/1-parser.ts
@@ -1,231 +1,9 @@
 import { Colour, Foundation, ParsedStitch, Pattern, StitchType } from '../types.js';
-
-enum TokenType {
-    Single = 'sc',
-    Double = 'dc',
-    Slip = 'sl',
-    Chain = 'ch',
-    Triple = 'tr',
-    Increment = 'inc',
-    Decrement = 'dec',
-    Invert = 'inv',
-    Comma = ',',
-    LParen = '(',
-    RParen = ')',
-    Number = 'number',
-    Colon = ':',
-    Asterisk = '*',
-    Colour = 'white',
-    Foundation = 'mr',
-    IntoClause = 'into',
-    Repeat = 'repeat',
-    From = 'from',
-    x = 'x',
-    More = 'more',
-    Times = 'times',
-}
-
-interface ParsedToken {
-    type: TokenType;
-    value: string | number;
-}
-
-function stringTokenLUT(input: string): ParsedToken {
-    // expects strings to be stripped of all punctuation and whitespace
-    input = input.toLowerCase();
-    // Things we understand but do not implement
-    const notImplementedLUT = new Set<string>(['sp', 'hdc', 'slst']);
-    if (notImplementedLUT.has(input)) {
-        throw new TypeError(`${input} is currently not supported!`);
-    }
-
-    // LUT for every valid stitch type
-    // Note that this should also include aliases (eg. ch and chain are both in the LUT)
-    const chainLUT = new Set<string>(['ch', 'chain']);
-    if (chainLUT.has(input)) {
-        return { type: TokenType.Chain, value: input };
-    }
-
-    const singleLUT = new Set<string>(['single', 'sc']);
-    if (singleLUT.has(input)) {
-        return { type: TokenType.Single, value: input };
-    }
-
-    const slipLUT = new Set<string>(['slip', 'sl', 'slipknot']);
-    if (slipLUT.has(input)) {
-        return { type: TokenType.Slip, value: input };
-    }
-
-    const doubleLUT = new Set<string>(['double', 'do']);
-    if (doubleLUT.has(input)) {
-        return { type: TokenType.Double, value: input };
-    }
-
-    const tripleLUT = new Set<string>(['triple', 'tr', 'treble']);
-    if (tripleLUT.has(input)) {
-        return { type: TokenType.Triple, value: input };
-    }
-
-    const incrementLUT = new Set<string>(['increment', 'inc']);
-    if (incrementLUT.has(input)) {
-        return { type: TokenType.Increment, value: input };
-    }
-    const decrementLUT = new Set<string>(['decrement', 'dec']);
-    if (decrementLUT.has(input)) {
-        return { type: TokenType.Decrement, value: input };
-    }
-    const invertLUT = new Set<string>(['invert', 'inv']);
-    if (invertLUT.has(input)) {
-        return { type: TokenType.Invert, value: input };
-    }
-
-    //add more colours here as desired
-    const colourLUT = new Set<string>([
-        'white',
-        'black',
-        'red',
-        'orange',
-        'yellow',
-        'green',
-        'blue',
-        'indigo',
-        'violet',
-    ]);
-    if (colourLUT.has(input)) {
-        return { type: TokenType.Colour, value: input };
-    }
-
-    // Note! if the foundation is specified as a slipknot,
-    // there's no way for us to tell the difference between specifying it
-    // as a stitch vs a foundation knot based on the grammar. As such we'll only catch magic ring
-    const foundationLUT = new Set<string>(['mr', 'magicring', 'magic', 'ring']);
-    if (foundationLUT.has(input)) {
-        return { type: TokenType.Foundation, value: 'mr' };
-    }
-
-    // This should simplify latter stages of the grammar
-    const countAlias = new Set<string>(['twice', 'thrice']);
-    if (countAlias.has(input)) {
-        if (input === 'twice') {
-            return { type: TokenType.Number, value: 2 };
-        }
-        if (input === 'thrice') {
-            return { type: TokenType.Number, value: 3 };
-        }
-    }
-
-    // Misc. aliases
-    const repeat = new Set<string>(['rep', 'repeat']);
-    if (repeat.has(input)) {
-        return { type: TokenType.Repeat, value: 'repeat' };
-    }
-
-    const times = new Set<string>(['time', 'times']);
-    if (times.has(input)) {
-        return { type: TokenType.Times, value: 'times' };
-    }
-
-    // Misc. cases
-    if (input === '*') {
-        return { type: TokenType.Asterisk, value: '*' };
-    }
-    if (input === 'x') {
-        return { type: TokenType.x, value: 'x' };
-    }
-    if (input === 'from') {
-        return { type: TokenType.From, value: 'from' };
-    }
-    if (input === 'more') {
-        return { type: TokenType.More, value: 'more' };
-    }
-
-    // throw new TypeError(`Unknown string token ${input}`)
-    console.warn(`Unknown string token ${input}`);
-    // debug fix CHANGE THIS
-    return { type: TokenType.Colon, value: input };
-}
-
-function tokenizer(input: string): ParsedToken[] {
-    input = input.replace(/\s+/g, ' ').trim();
-    let output: ParsedToken[] = [];
-    let tokens = input.split(' ');
-    for (let i = 0; i < tokens.length; i++) {
-        let val = tokens[i];
-        let endTokens: ParsedToken[] = [];
-        while (val.length > 0) {
-            // strip off any leading brackets (only type of leading thing according to spec)
-            if (val[0] == '(') {
-                output.push({ type: TokenType.LParen, value: '(' });
-                val = val.substring(1);
-                continue;
-            }
-            if (val[0] == '{') {
-                output.push({ type: TokenType.LParen, value: '{' });
-                val = val.substring(1);
-                continue;
-            }
-            if (val[0] == '[') {
-                output.push({ type: TokenType.LParen, value: '{' });
-                val = val.substring(1);
-                continue;
-            }
-            //strip off trailing colons, rparens, or commas (only type of trailing according to spec)
-            if (val.slice(-1) == ')') {
-                endTokens.push({ type: TokenType.RParen, value: ')' });
-                val = val.substring(0, val.length - 1);
-                continue;
-            }
-            if (val.slice(-1) == '}') {
-                endTokens.push({ type: TokenType.RParen, value: '}' });
-                val = val.substring(0, val.length - 1);
-                continue;
-            }
-            if (val.slice(-1) == ']') {
-                endTokens.push({ type: TokenType.RParen, value: '}' });
-                val = val.substring(0, val.length - 1);
-                continue;
-            }
-            if (val.slice(-1) == ':') {
-                endTokens.push({ type: TokenType.Colon, value: ':' });
-                val = val.substring(0, val.length - 1);
-                continue;
-            }
-            if (val.slice(-1) == ',') {
-                endTokens.push({ type: TokenType.Comma, value: ',' });
-                val = val.substring(0, val.length - 1);
-                continue;
-            }
-            // console.log(`sliced ${tokens[i]} to ${val}`)
-            break;
-        }
-
-        // TODO: Split strings here (ch20 is valid technically)
-
-        // So let's try parsing it as an int
-        let numberCandidate = parseInt(val);
-        if (Number.isNaN(numberCandidate)) {
-            // LUT for valid strings
-            endTokens.push(stringTokenLUT(val));
-        } else {
-            // If it is a negative number throw exn
-            if (numberCandidate < 1) {
-                throw new RangeError(`Repeat counts or stitch counts must be positive integers! 
-                                        Found zero or negative value ${numberCandidate}`);
-            }
-            endTokens.push({ type: TokenType.Number, value: numberCandidate });
-        }
-
-        for (let i = endTokens.length - 1; i >= 0; i--) {
-            output.push(endTokens[i]);
-        }
-    }
-    return output;
-}
+import { Keyword, lex } from './1-parser/lexer.js';
 
 export function parse(input: string): Pattern<ParsedStitch> {
-    let tokens = tokenizer(input);
+    const tokens = lex(input);
     console.log(tokens);
-    // console.log("Completed tokenization")
     let foundation = Foundation.SlipKnot;
     let colour = Colour.White;
 
@@ -235,41 +13,39 @@ export function parse(input: string): Pattern<ParsedStitch> {
     // parser which ignores repeats
     for (let i = 0; i < tokens.length; i++) {
         let token = tokens[i];
-        if (token.type == TokenType.Colour) {
+        if (token.type === 'color') {
             colour = Colour[token.value as keyof typeof Colour];
-        }
-        if (token.type == TokenType.Chain) {
-            curStitchType = StitchType.Chain;
-        }
-        if (token.type == TokenType.Single) {
-            curStitchType = StitchType.Single;
-        }
-        if (token.type == TokenType.Double) {
-            curStitchType = StitchType.Double;
-        }
-        if (token.type == TokenType.Slip) {
-            curStitchType = StitchType.Slip;
-        }
-        if (token.type == TokenType.Triple) {
-            curStitchType = StitchType.Treble;
-        }
-        if (token.type == TokenType.Number) {
-            curStitchCount = token.value as number;
-        }
-        if (token.type == TokenType.Comma) {
-            if (curStitchType != null && curStitchCount != null) {
-                stitches.push({
-                    type: curStitchType,
-                    repeat: curStitchCount,
-                    colour: colour,
-                    into: null,
-                });
+        } else if (token.type === 'keyword') {
+            if (token.value === Keyword.Chain) {
+                curStitchType = StitchType.Chain;
+            } else if (token.value === Keyword.Single) {
+                curStitchType = StitchType.Single;
+            } else if (token.value === Keyword.Double) {
+                curStitchType = StitchType.Double;
+            } else if (token.value === Keyword.Slip) {
+                curStitchType = StitchType.Slip;
+            } else if (token.value === Keyword.Treble) {
+                curStitchType = StitchType.Treble;
             }
-            curStitchCount = null;
-            curStitchType = null;
+        } else if (token.type === 'number') {
+            curStitchCount = token.value;
+        } else if (token.type === 'symbol') {
+            if (token.value === ',') {
+                if (curStitchType != null && curStitchCount != null) {
+                    stitches.push({
+                        type: curStitchType,
+                        repeat: curStitchCount,
+                        colour: colour,
+                        into: null,
+                    });
+                }
+                curStitchCount = null;
+                curStitchType = null;
+            }
         }
     }
-    if (curStitchType != null && curStitchCount != null) {
+
+    if (curStitchType !== null && curStitchCount !== null) {
         stitches.push({ type: curStitchType, repeat: curStitchCount, colour: colour, into: null });
     }
 
---
+++ crochet-stitcher/src/stages/1-parser/lexer.ts
@@ -0,0 +1,95 @@
+export enum Keyword {
+    // Generic words:
+    Stitch = 'Stitch',
+    // Foundations:
+    MagicRing = 'MagicRing',
+    // Stitches:
+    Chain = 'Chain',
+    Slip = 'Slip',
+    Single = 'Single',
+    Double = 'Double',
+    HalfDouble = 'HalfDouble',
+    Treble = 'Treble',
+    Increase = 'Increase',
+    Decrease = 'Decrease',
+    // Into-clauses:
+    Into = 'Into',
+    Next = 'Next',
+    // Repeating:
+    Repeat = 'Repeat',
+    From = 'From',
+    Twice = 'Twice',
+    Thrice = 'Thrice',
+    Times = 'Times',
+    More = 'More',
+}
+
+export type Token =
+    | { type: 'keyword'; value: Keyword }
+    | { type: 'color'; value: string }
+    | { type: 'symbol'; value: string }
+    | { type: 'number'; value: number };
+
+export function lex(input: string): Token[] {
+    const tokens: Token[] = [];
+
+    const regex = /\s*([A-Za-z_]+|\d+|\S)/g;
+    while (true) {
+        const match = regex.exec(input);
+        if (!match) break;
+        const value = match[1];
+
+        if (KEYWORD_LUT[value]) {
+            tokens.push({ type: 'keyword', value: KEYWORD_LUT[value] });
+        } else if (COLOR_LUT.has(value)) {
+            tokens.push({ type: 'color', value });
+        } else if (/^\d+$/.test(value)) {
+            tokens.push({ type: 'number', value: parseInt(value) });
+        } else if (/^\W$/.test(value)) {
+            tokens.push({ type: 'symbol', value });
+        } else {
+            throw Error(`Unrecognized token '${value}'`);
+        }
+    }
+
+    return tokens;
+}
+
+const KEYWORD_NAMES: Record<Keyword, string[]> = {
+    [Keyword.Stitch]: ['stitch', 'st'],
+    [Keyword.MagicRing]: ['mr', 'mc'],
+    [Keyword.Chain]: ['chain', 'ch'],
+    [Keyword.Slip]: ['slip', 'sl', 'slst', 'ss'],
+    [Keyword.Single]: ['single', 'sc'],
+    [Keyword.Double]: ['double', 'dc'],
+    [Keyword.HalfDouble]: ['hdc'],
+    [Keyword.Treble]: ['treble', 'triple', 'tr', 'tc'],
+    [Keyword.Increase]: ['increase', 'inc'],
+    [Keyword.Decrease]: ['decrease', 'dec'],
+    [Keyword.Into]: ['into', 'in'],
+    [Keyword.Next]: ['next'],
+    [Keyword.Repeat]: ['repeat', 'rep'],
+    [Keyword.From]: ['from'],
+    [Keyword.Twice]: ['twice', 'two'],
+    [Keyword.Thrice]: ['thrice', 'three'],
+    [Keyword.Times]: ['times', 'time', 'x'],
+    [Keyword.More]: ['more'],
+};
+
+const KEYWORD_LUT: Record<string, Keyword> = Object.fromEntries(
+    Object.entries(KEYWORD_NAMES).flatMap(([keyword, names]) =>
+        names.map((name) => [name, keyword as Keyword]),
+    ),
+);
+
+const COLOR_LUT = new Set<string>([
+    'white',
+    'black',
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'indigo',
+    'violet',
+]);
