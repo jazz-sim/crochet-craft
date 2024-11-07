d04ab10dd4efcb61d10eefd8d3ba5d77b1181346
simcard0000 <simran.thind@outlook.com>
2024-11-07 15:13:23 -0500

    fix up Editor and Properties panels

--- crochetcraft/src/components/option-panel/Panel.svelte
+++ crochetcraft/src/components/option-panel/Panel.svelte
@@ -1,16 +1,22 @@
-<div class="overlay">
-    <slot />
+<div class="overlay card">
+    <div class="bar card p-4"></div>
+    <header class="card-header"><slot name="panel-title" /></header>
+    <section class="p-4"><slot name="panel-elements" /></section>
 </div>
 
 <style>
     .overlay {
-        position: fixed;
-        top: 10px;
-        left: 10px;
+        position: absolute;
         z-index: 1; /* Ensure it's above the canvas */
-        background-color: rgba(255, 255, 255, 0.8); /* Transparent background */
-        padding: 10px;
-        border: 1px solid #ccc;
         border-radius: 8px;
+        width: 400px;
+        top: var(--top, 100px);
+        left: var(--left, 10px);
+    }
+    .bar {
+        background-color: grey;
+        border-top-left-radius: 8px;
+        border-top-right-radius: 8px;
+        padding: none;
     }
 </style>
