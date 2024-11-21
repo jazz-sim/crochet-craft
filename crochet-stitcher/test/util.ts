a75b172f5b97f873884965aa4b022287886a32dc
Jasmine Tai <jtai@jtai.ca>
2024-11-21 14:01:00 -0500

    Implement 'turn' and end-of-row markers in parser

--- crochet-stitcher/test/util.ts
+++ crochet-stitcher/test/util.ts
@@ -1,11 +1,13 @@
 import { Pattern, Foundation } from '../src/types.js';
 
+export type MaybeArray<T> = T | MaybeArray<T>[];
+
 /** A shortcut to create a slip-knot pattern. */
-export function slkt<T>(...stitches: T[]): Pattern<T extends readonly (infer U)[] ? U : T> {
-    return { foundation: Foundation.SlipKnot, stitches: stitches.flat() };
+export function slkt<T>(...stitches: MaybeArray<T>[]): Pattern<T> {
+    return { foundation: Foundation.SlipKnot, stitches: (stitches as any[]).flat(Infinity) };
 }
 
 /** A shortcut to create a magic-ring pattern. */
-export function mc<T>(...stitches: T[]): Pattern<T extends readonly (infer U)[] ? U : T> {
-    return { foundation: Foundation.MagicRing, stitches: stitches.flat() };
+export function mc<T>(...stitches: MaybeArray<T>[]): Pattern<T> {
+    return { foundation: Foundation.MagicRing, stitches: (stitches as any[]).flat(Infinity) };
 }
