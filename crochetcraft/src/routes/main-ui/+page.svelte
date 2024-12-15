fd3dcd829a02b7900a064f0f70fc333f34adf8ea
Jasmine Tai <jtai@jtai.ca>
2024-12-14 20:24:09 -0800

    Docked sidebars (experiment)
    This experiment makes the two sidebars (pattern sidebar and properties
    sidebar) "docked"; that is, they are no longer movable, and they no
    longer overlap the canvas.

--- crochetcraft/src/routes/main-ui/+page.svelte
+++ crochetcraft/src/routes/main-ui/+page.svelte
@@ -3,15 +3,13 @@
     import Editor from '$components/main-ui/Editor.svelte';
     import Properties from '$components/main-ui/Properties.svelte';
     import Toolbar from '$components/main-ui/Toolbar.svelte';
-
-    let toolbar: Toolbar;
 </script>
 
 <div class="flex h-full flex-col">
-    <Toolbar bind:this={toolbar} />
-    <div id="render-div" class="relative min-h-0 flex-1">
+    <Toolbar />
+    <div id="render-div" class="relative flex min-h-0 flex-1 flex-row">
+        <Editor position="docked" />
         <Canvas />
-        <Editor />
-        <Properties />
+        <Properties position="docked" />
     </div>
 </div>
