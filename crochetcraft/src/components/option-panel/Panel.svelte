6f53cd436a1aba50ce88c33921c041d7ca79ad4b
Jasmine Tai <jtai@jtai.ca>
2024-12-14 20:24:09 -0800

    Docked sidebars (experiment)
    This experiment makes the two sidebars (pattern sidebar and properties
    sidebar) "docked"; that is, they are no longer movable, and they no
    longer overlap the canvas.

--- crochetcraft/src/components/option-panel/Panel.svelte
+++ crochetcraft/src/components/option-panel/Panel.svelte
@@ -1,3 +1,7 @@
+<script module lang="ts">
+    export type PanelPosition = 'left' | 'right' | 'docked';
+</script>
+
 <script lang="ts">
     import { draggable } from '@neodrag/svelte';
     import plus from '$lib/assets/white-plus.png';
@@ -12,32 +16,45 @@
     }: {
         children: Snippet;
         title: string;
-        position: 'left' | 'right';
+        position: PanelPosition;
         shown?: boolean;
     } = $props();
 
     let expanded = $state(true);
 </script>
 
-<div
-    use:draggable={{ bounds: '#render-div', handle: '.panel-bar' }}
-    class="card absolute top-2 z-10 w-96 rounded-lg"
-    class:left-2={position === 'left'}
-    class:right-2={position === 'right'}
-    class:hidden={!shown}
->
+{#snippet cardContent()}
     <div
-        class="panel-bar variant-soft-primary card flex cursor-move flex-row justify-between rounded-t-lg p-4"
+        class="panel-bar variant-soft-primary card flex cursor-move flex-row justify-between p-4"
+        class:rounded-t-lg={position !== 'docked'}
     >
         <h4 class="h4">{title}</h4>
-        <button
-            class="btn-icon btn-icon-sm variant-filled-surface justify-end rounded-lg"
-            title={expanded ? 'Minimize' : 'Maximize'}
-            aria-label={expanded ? 'Minimize' : 'Maximize'}
-            onclick={() => (expanded = !expanded)}
-        >
-            <img src={expanded ? minus : plus} alt={expanded ? 'Minimize' : 'Maximize'} />
-        </button>
+        {#if position !== 'docked'}
+            <button
+                class="btn-icon btn-icon-sm variant-filled-surface justify-end rounded-lg"
+                title={expanded ? 'Minimize' : 'Maximize'}
+                aria-label={expanded ? 'Minimize' : 'Maximize'}
+                onclick={() => (expanded = !expanded)}
+            >
+                <img src={expanded ? minus : plus} alt={expanded ? 'Minimize' : 'Maximize'} />
+            </button>
+        {/if}
     </div>
     <div class="space-y-4 p-4" class:hidden={!expanded}>{@render children()}</div>
-</div>
+{/snippet}
+
+{#if position === 'docked'}
+    <div class="card z-10 w-96 flex-none">
+        {@render cardContent()}
+    </div>
+{:else}
+    <div
+        use:draggable={{ bounds: '#render-div', handle: '.panel-bar' }}
+        class="card absolute top-2 z-10 w-96 flex-none rounded-lg"
+        class:left-2={position === 'left'}
+        class:right-2={position === 'right'}
+        class:hidden={!shown}
+    >
+        {@render cardContent()}
+    </div>
+{/if}
