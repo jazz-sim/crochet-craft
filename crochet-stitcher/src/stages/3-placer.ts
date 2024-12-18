3b26d8b7afc137c92a876e57dea19f3af4f53d4c
AltheFlyer <30613228+AltheFlyer@users.noreply.github.com>
2024-12-17 19:09:53 -0500

    largely untested placer estimator

--- crochet-stitcher/src/stages/3-placer.ts
+++ crochet-stitcher/src/stages/3-placer.ts
@@ -1,36 +1,14 @@
 import { Vector3 } from 'three';
 import { LinkedStitch, Pattern, PlacedStitch } from '../types.js';
+import { naivePlacer } from './3-placer/naivePlacerEstimator.js';
 
 export function place(input: Pattern<LinkedStitch>): Pattern<PlacedStitch> {
-    throw 0; // TODO
-}
+    const initialPlacement = naivePlacer(input);
 
-/**
- * Returns the force on the x, y, and z axes given a vector expressed in x/y/z and the stitch radii
- * for the two stitches using simple spring force formula using the sum of the stitch radii as the
- * "default" distance of the spring
- *
- * @param v The position of the other stitch relative to the one that force is being applied to
- * @param ownStitchRadius the radius of the stitch to apply force to
- * @param otherStitchRadius the radius of the stitch that is applying force
- * @returns A THREE.Vector3-compatible 3D vector containing the force to apply.
- */
-export function evaluateForce(
-    v: Vector3,
-    ownStitchRadius: number,
-    otherStitchRadius: number,
-): Vector3 {
-    // "spring constant", adjust as needed
-    // Allen's note: this spring constant is kind of magical and just worked.
-    // I have no idea how Osman magicked this one but it's a pretty good choice.
-    const k = 0.5;
-    let distance = Math.sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
-    let neutralDistance = ownStitchRadius + otherStitchRadius;
-    let diffFromNeutral = distance - neutralDistance;
-    let netForce = diffFromNeutral * k;
-    let vUnit = { x: v.x / distance, y: v.y / distance, z: v.z / distance };
-    let forceXYZ = new Vector3(netForce * vUnit.x, netForce * vUnit.y, netForce * vUnit.z);
-    return forceXYZ;
+    return {
+        foundation: input.foundation,
+        stitches: initialPlacement,
+    };
 }
 
 /**
