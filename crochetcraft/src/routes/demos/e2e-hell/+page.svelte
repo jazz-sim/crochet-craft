8240d877b699550fd5ea3846f580c2446e7cbbb2
AltheFlyer <30613228+AltheFlyer@users.noreply.github.com>
2024-12-17 19:57:20 -0500

    more examples for e2e page

--- crochetcraft/src/routes/demos/e2e-hell/+page.svelte
+++ crochetcraft/src/routes/demos/e2e-hell/+page.svelte
@@ -9,15 +9,23 @@
     let scene: THREE.Scene;
     let mainGroup: THREE.Group = new THREE.Group();
 
-    const sampleInputs: string[] = ['ch 3, turn, sc 3'];
-    const placedPoints = place(link(parse(sampleInputs[0])));
-
-    placeDebugSpheres(
-        placedPoints.stitches.map((p) => new Vector3(p.position.x, p.position.y, p.position.z)),
-        mainGroup,
-    );
+    const sampleInputs: { [key: string]: string } = {
+        'default chain': 'ch 10',
+        'long chain': 'ch 100',
+        '3x3 square': 'ch 3, turn, sc 3, turn, sc3',
+    };
+    const sampleNames = Object.keys(sampleInputs);
+    let selectedSampleName = 'default chain';
 
     $: {
+        const currentSample = sampleInputs[selectedSampleName];
+        const placedPoints = place(link(parse(currentSample)));
+
+        mainGroup.clear();
+        placeDebugSpheres(
+            placedPoints.stitches.map((p) => new Vector3(p.position.x, p.position.y, p.position.z)),
+            mainGroup,
+        );
         scene?.add(mainGroup);
     }
 </script>
@@ -28,6 +36,12 @@
     <div id="input-wrapper">
         <a class="anchor" href={`/demos`}>📺 Back To Demos</a>
         <br /><br />
+        <p><i>Demonstrations:</i></p>
+        <select class="select" bind:value={selectedSampleName}>
+            {#each sampleNames as sampleStr}
+                <option value={sampleStr}>{sampleStr}</option>
+            {/each}
+        </select>
     </div>
 </div>
 
