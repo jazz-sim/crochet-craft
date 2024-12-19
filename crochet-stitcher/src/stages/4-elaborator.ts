fbe949bf9c407ad7f93b7c8dee5610ecfc4be15d
AltheFlyer <30613228+AltheFlyer@users.noreply.github.com>
2024-12-18 21:52:39 -0500

    copy paste elaborator (only chain)

--- crochet-stitcher/src/stages/4-elaborator.ts
+++ crochet-stitcher/src/stages/4-elaborator.ts
@@ -1,5 +1,9 @@
 import { Pattern, PlacedStitch, Point } from '../types.js';
+import { copyPasteStitches } from './4-elaborator/copyPasteElaborator.js';
+import { Mesh, BufferGeometry, NormalBufferAttributes, Material, Object3DEventMap } from 'three';
 
-export function elaborate(input: Pattern<PlacedStitch>): Pattern<Point> {
-    throw 0; // TODO
+export function elaborate(
+    input: Pattern<PlacedStitch>,
+): Mesh<BufferGeometry<NormalBufferAttributes>, Material | Material[], Object3DEventMap>[] {
+    return copyPasteStitches(input);
 }
