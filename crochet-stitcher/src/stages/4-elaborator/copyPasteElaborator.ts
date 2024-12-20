837f39e91b6598c4a17e3dec31607cdf0a8f4d19
AltheFlyer <30613228+AltheFlyer@users.noreply.github.com>
2024-12-19 20:56:27 -0500

    fix continuity between stitches

--- crochet-stitcher/src/stages/4-elaborator/copyPasteElaborator.ts
+++ crochet-stitcher/src/stages/4-elaborator/copyPasteElaborator.ts
@@ -32,15 +32,33 @@ import {
 export function copyPasteStitches(
     placedStitches: Pattern<PlacedStitch>,
 ): Mesh<BufferGeometry<NormalBufferAttributes>, Material | Material[], Object3DEventMap>[] {
-    return placedStitches.stitches
-        .map((stitch) => {
-            return {
-                ...stitch,
-                ...substituteStitch(stitch),
-            };
-        })
-        .map(movePointsToPlacement)
-        .flatMap(curveToGeometries);
+    return (
+        placedStitches.stitches
+            // "Copy-paste" step; for each placed stitch,
+            // add the points from the base model corresponding to its type
+            .map((stitch) => {
+                return {
+                    ...stitch,
+                    ...substituteStitch(stitch),
+                };
+            })
+            // Base models (supposedly) are centered around the origin.
+            // This moves them to where the placer thinks they should be
+            .map(movePointsToPlacement)
+            // Make stitches connect to each other, and not extremely jagged
+            .map((stitch, index, arr) => {
+                if (
+                    index == 0 ||
+                    !(stitch.curveType == 'bezier' && arr[index - 1].curveType == 'bezier')
+                ) {
+                    return stitch;
+                }
+                return startToEndFixer(stitch, arr[index - 1]);
+            })
+            // Generate geometries
+            // (This could be moved to crochetcraft)
+            .flatMap(curveToGeometries)
+    );
 }
 
 type IntermediatePlacement = PlacedStitch & StitchModel;
@@ -79,6 +97,69 @@ function movePointsToPlacement(stitch: IntermediatePlacement): IntermediatePlace
             return [rx + stitch.position.x, ry + stitch.position.y, rz + stitch.position.z];
         }),
     };
+}
+
+/**
+ * Sets the starting point of `nextStitch` to the ending point of `prevStitch`,
+ * and modifies the first anchor of `nextStitch` to be aligned with the
+ * last anchor of `prevStitch`.
+ *
+ * This ensures (bezier-curve based) stitches are continuous, and first-derivative continuous.
+ *
+ * @param nextStitch the stitch whose starting point to modify ("fix")
+ * @param prevStitch the stitch whose ending point is used to modify `nextStitch`
+ */
+function startToEndFixer(
+    nextStitch: IntermediatePlacement,
+    prevStitch: IntermediatePlacement,
+): IntermediatePlacement {
+    // Move end to start
+    const endOfPrevStitch = prevStitch.points[prevStitch.points.length - 1];
+    // Deep copy to prevent reference jank
+    nextStitch.points[0] = [endOfPrevStitch[0], endOfPrevStitch[1], endOfPrevStitch[2]];
+
+    // Reorient anchor for continuity
+    // On the one hand, this does make the chain stitch look much better.
+    // On the other, this is a lot of work to fix an issue with the original base model.
+    // (...which is my fault for the record - Allen)
+    // But on the other other hand, it's unreasonable for every base model to end in the same direction.
+
+    // Find the direction of the end of the previous stitch
+    // i.e. what way it would go if you extended it along a line
+    const prevAnchor = prevStitch.points[prevStitch.points.length - 2];
+    const prevAnchorDiff = [
+        endOfPrevStitch[0] - prevAnchor[0],
+        endOfPrevStitch[1] - prevAnchor[1],
+        endOfPrevStitch[2] - prevAnchor[2],
+    ];
+    const r = Math.sqrt(
+        prevAnchorDiff[0] * prevAnchorDiff[0] +
+            prevAnchorDiff[1] * prevAnchorDiff[1] +
+            prevAnchorDiff[2] * prevAnchorDiff[2],
+    );
+    const direction = [prevAnchorDiff[0] / r, prevAnchorDiff[1] / r, prevAnchorDiff[2] / r];
+
+    // Get the distance of the first anchor from the first point of the next stitch
+    const nextAnchorDiff = [
+        nextStitch.points[1][0] - nextStitch.points[0][0],
+        nextStitch.points[1][1] - nextStitch.points[0][1],
+        nextStitch.points[1][2] - nextStitch.points[0][2],
+    ];
+    const distance = Math.sqrt(
+        nextAnchorDiff[0] * nextAnchorDiff[0] +
+            nextAnchorDiff[1] * nextAnchorDiff[1] +
+            nextAnchorDiff[2] * nextAnchorDiff[2],
+    );
+
+    // Modify the first anchor to be aligned in the same direction as the
+    // last anchor of the previous stitch.
+    nextStitch.points[1] = [
+        nextStitch.points[0][0] + direction[0] * distance,
+        nextStitch.points[0][1] + direction[1] * distance,
+        nextStitch.points[0][2] + direction[2] * distance,
+    ];
+
+    return nextStitch;
 }
 
 /**
