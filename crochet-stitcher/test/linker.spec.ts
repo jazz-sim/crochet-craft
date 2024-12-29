838489ebdeb3ec2913c7a79280b0e401fb65e91a
Jasmine Tai <jtai@jtai.ca>
2024-12-28 17:00:23 -0800

    Refactor linker unit tests

--- crochet-stitcher/test/linker.spec.ts
+++ crochet-stitcher/test/linker.spec.ts
@@ -1,70 +1,44 @@
 import { link } from '../src/stages/2-linker';
-import { Foundation, ParsedInstruction, ParsedStitch, StitchType } from '../src/types';
+import { LinkedStitch, ParsedInstruction, StitchType } from '../src/types';
+import { lst } from './linker-util.js';
 import { st } from './parser.js';
 import { mc as rawMc, slkt as rawSlkt } from './util.js';
 
 const mc = rawMc<ParsedInstruction>;
 const slkt = rawSlkt<ParsedInstruction>;
-
-const chainLink = {
-    parent: null,
-    type: StitchType.Chain,
-};
-
-function singleLink(parent: number) {
-    return {
-        parent: parent,
-        type: StitchType.Single,
-    };
-}
-
-function withDefOpt(stitch: any) {
-    return {
-        colour: 'white',
-        ...stitch,
-    };
-}
-
-function stitchesWithDefOpt(stitches: any[]) {
-    return stitches.map(withDefOpt);
-}
+const linkedMc = rawMc<LinkedStitch>;
+const linkedSlkt = rawSlkt<LinkedStitch>;
 
 describe('basic linking', () => {
     test('minimal stitch', () => {
-        expect(link(slkt(st(StitchType.Chain, 1)))).toEqual({
-            foundation: Foundation.SlipKnot,
-            stitches: [withDefOpt(chainLink)],
-        });
+        expect(link(slkt(st(StitchType.Chain, 1)))).toEqual(linkedSlkt(lst(StitchType.Chain)));
     });
 
     test('basic chain', () => {
-        expect(link(slkt(st(StitchType.Chain, 3)))).toEqual({
-            foundation: Foundation.SlipKnot,
-            stitches: stitchesWithDefOpt([chainLink, chainLink, chainLink]),
-        });
+        expect(link(slkt(st(StitchType.Chain, 3)))).toEqual(
+            linkedSlkt(lst(StitchType.Chain), lst(StitchType.Chain), lst(StitchType.Chain)),
+        );
     });
 
     test('magic ring', () => {
-        expect(link(mc(st(StitchType.Chain, 3), 'eor'))).toEqual({
-            foundation: Foundation.MagicRing,
-            stitches: stitchesWithDefOpt([chainLink, chainLink, chainLink]),
-        });
+        expect(link(mc(st(StitchType.Chain, 3), 'eor'))).toEqual(
+            linkedMc(lst(StitchType.Chain), lst(StitchType.Chain), lst(StitchType.Chain)),
+        );
     });
 });
 
 describe('multiple row linking', () => {
     test('two rows', () => {
-        expect(link(slkt(st(StitchType.Chain, 3), 'eor', st(StitchType.Single, 3)))).toEqual({
-            foundation: Foundation.SlipKnot,
-            stitches: stitchesWithDefOpt([
-                chainLink,
-                chainLink,
-                chainLink,
-                singleLink(0),
-                singleLink(1),
-                singleLink(2),
-            ]),
-        });
+        expect(link(slkt(st(StitchType.Chain, 3), 'eor', st(StitchType.Single, 3)))).toEqual(
+            linkedSlkt(
+                lst(StitchType.Chain),
+                lst(StitchType.Chain),
+                lst(StitchType.Chain),
+                lst(StitchType.Single, 0),
+                lst(StitchType.Single, 1),
+                lst(StitchType.Single, 2),
+            ),
+        );
     });
 
     test('3x3 square', () => {
@@ -79,21 +53,20 @@ describe('multiple row linking', () => {
                     st(StitchType.Single, 3),
                 ),
             ),
-        ).toEqual({
-            foundation: Foundation.SlipKnot,
-            stitches: stitchesWithDefOpt([
-                chainLink,
-                chainLink,
-                chainLink,
-                chainLink,
-                singleLink(2),
-                singleLink(1),
-                singleLink(0),
-                singleLink(6),
-                singleLink(5),
-                singleLink(4),
-            ]),
-        });
+        ).toEqual(
+            linkedSlkt(
+                lst(StitchType.Chain),
+                lst(StitchType.Chain),
+                lst(StitchType.Chain),
+                lst(StitchType.Chain),
+                lst(StitchType.Single, 2),
+                lst(StitchType.Single, 1),
+                lst(StitchType.Single, 0),
+                lst(StitchType.Single, 6),
+                lst(StitchType.Single, 5),
+                lst(StitchType.Single, 4),
+            ),
+        );
         /* Visually, we should get something like:
          7  8  9
 		 6  5  4 (3)
