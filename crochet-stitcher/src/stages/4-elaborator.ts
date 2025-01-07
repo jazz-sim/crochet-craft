52e294b652124d785de6564524933ce478b7836e
Jasmine Tai <jtai@jtai.ca>
2025-01-07 15:58:12 -0500

    Add gradient-descent-based placer

--- crochet-stitcher/src/stages/4-elaborator.ts
+++ crochet-stitcher/src/stages/4-elaborator.ts
@@ -1,6 +1,6 @@
+import { BufferGeometry, Material, Mesh, NormalBufferAttributes, Object3DEventMap } from 'three';
 import { Pattern, PlacedStitch } from '../types.js';
 import { copyPasteStitches } from './4-elaborator/copyPasteElaborator.js';
-import { Mesh, BufferGeometry, NormalBufferAttributes, Material, Object3DEventMap } from 'three';
 
 export function elaborate(
     input: Pattern<PlacedStitch>,
