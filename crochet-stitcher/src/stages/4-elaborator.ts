e0a617aa68fd2b517736e4ae0727ee3f94a8e88a
AltheFlyer <30613228+AltheFlyer@users.noreply.github.com>
2025-01-18 15:16:17 -0500

    make pipeline slightly less painful to use

--- crochet-stitcher/src/stages/4-elaborator.ts
+++ crochet-stitcher/src/stages/4-elaborator.ts
@@ -3,12 +3,23 @@ import { copyPasteStitches } from './4-elaborator/copyPasteElaborator.js';
 import { Mesh, BufferGeometry, NormalBufferAttributes, Material, Object3DEventMap } from 'three';
 import { generateGeometry } from './4-elaborator/geometryGen.js';
 import { smoothConnections } from './4-elaborator/smoother.js';
+import { PatternIR } from './4-elaborator/ir.js';
+
+const pipelineSteps: ((pre: PatternIR) => PatternIR)[] = [smoothConnections];
 
 export function elaborate(
     input: Pattern<PlacedStitch>,
 ): Mesh<BufferGeometry<NormalBufferAttributes>, Material | Material[], Object3DEventMap>[] {
-    const copyPasteOutput = copyPasteStitches(input);
-    const smoothedOutput = smoothConnections(copyPasteOutput);
-    const outputGeometry = generateGeometry(smoothedOutput);
+    // Inital Step: Convert placed patterns into Elaborator IR
+    let intermediateRepresentation = copyPasteStitches(input);
+
+    // Pipeline: run each pass in the order defined by `pipelineSteps`
+    pipelineSteps.forEach((step) => {
+        intermediateRepresentation = step(intermediateRepresentation);
+    });
+
+    // Final Step: convert pipeline result into a THREE.js geometry that
+    // can be rendered by CrochetCraft directly.
+    const outputGeometry = generateGeometry(intermediateRepresentation);
     return outputGeometry;
 }
