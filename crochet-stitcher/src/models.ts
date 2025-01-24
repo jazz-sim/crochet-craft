5f80750bf0a5c9a4b5c6bd98b39c6ca6db3669ea
Carolx715 <carolx715@gmail.com>
2025-01-23 20:39:32 -0500

    add initial magic circle

--- crochet-stitcher/src/models.ts
+++ crochet-stitcher/src/models.ts
@@ -1,6 +1,7 @@
 import chain from './models/chain.json';
 import slipKnot from './models/slip-knot.json';
 import single from './models/single.json';
+import magicCircle from './models/magic-circle.json';
 
 interface StitchModel {
     curveType: 'bezier';
@@ -11,6 +12,7 @@ const StitchModel = {
     CHAIN: chain as StitchModel,
     SLIP_KNOT: slipKnot as StitchModel,
     SINGLE_CROCHET: single as StitchModel,
+    MAGIC_CIRCLE: magicCircle as StitchModel,
 };
 
 export default StitchModel;
