8d5d29fdcbf64e7220dc832d25de9f3f24b1e7bc
AltheFlyer <30613228+AltheFlyer@users.noreply.github.com>
2025-02-19 22:44:18 -0500

    Fix orientation in -x direction

--- crochet-stitcher/src/stages/3-placer/orientationSetter.ts
+++ crochet-stitcher/src/stages/3-placer/orientationSetter.ts
@@ -2,6 +2,7 @@ import { Quaternion, Vector3 } from 'three';
 import { Pattern, PlacedStitch } from '../../types';
 
 const X_AXIS = new Vector3(1, 0, 0);
+const NEG_X_AXIS = new Vector3(-1, 0, 0);
 
 /**
  * Sets orientation of stitches based on the position of the following stitch.
@@ -32,5 +33,21 @@ export function setOrientations(pattern: Pattern<PlacedStitch>): Pattern<PlacedS
  * @param to the target direction
  */
 function rotationToDirection(from: Vector3, to: Vector3): Quaternion {
+    // If the target direction is in the negative x-direction, we define the orientation as a
+    // combination of two rotations, since using a single rotation would flip the stitch upside down:
+    // 1. Rotation around the Y axis to the -x direction
+    // 2. Rotation from the -x direction to the actual target
+    if (to.x < 0) {
+        // Quaternion multiplication is actually right to left, so the rotations are defined in reverse order here
+        return new Quaternion().multiplyQuaternions(
+            new Quaternion().setFromUnitVectors(NEG_X_AXIS, to.clone().normalize()),
+            // We could explicitly define a rotation around the Y axis using .setFromAxisAngle()
+            // since we should only ever call this with from = X_AXIS, but just in case from is something different,
+            // this should still work.
+            // (For some reason, setFromUnitVectors choose a "nice" axis of rotation for this case)
+            new Quaternion().setFromUnitVectors(from.normalize(), NEG_X_AXIS),
+        );
+    }
+
     return new Quaternion().setFromUnitVectors(from.clone().normalize(), to.clone().normalize());
 }
