81ef48234d74555bb7affbe44a639a2a8b6e3337
simcard0000 <simran.thind@outlook.com>
2025-03-07 22:40:40 -0500

    add more info to about modal

---
+++ crochetcraft/src/components/icon-card/IconCard.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import type { Snippet } from 'svelte';
+    let {
+        icon,
+        title,
+        content,
+        variant = 'variant-ghost-primary',
+    }: {
+        icon: string;
+        title: string;
+        content: Snippet;
+        variant?: string;
+    } = $props();
+</script>
+
+<div class={'card mt-2 flex w-full flex-col rounded-lg p-4 '.concat(variant)}>
+    <div class="flex flex-row items-center gap-2">
+        <span class={'badge-icon '.concat(variant)}><i class={icon}></i></span>
+        <b>{title}</b>
+    </div>
+    <div class="flex flex-row gap-7">
+        <div class="indent"></div>
+        <div class="content">{@render content()}</div>
+    </div>
+</div>
--- crochetcraft/src/components/modals/AboutModal.svelte
+++ crochetcraft/src/components/modals/AboutModal.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import type { SvelteComponent } from 'svelte';
+    import IconCard from '$components/icon-card/IconCard.svelte';
 
     // Style + variables for the modal:
     const cBase = 'card p-4 w-modal shadow-xl space-y-4 rounded-lg';
@@ -8,6 +9,41 @@
     export let parent: SvelteComponent;
 </script>
 
+{#snippet use()}
+    <p>
+        Check out the user guide on the <a
+            class="anchor"
+            href="https://github.com/jazz-sim/crochet-craft/wiki">CrochetCraft GitHub wiki</a
+        >!
+    </p>
+{/snippet}
+
+{#snippet contribute()}
+    <p>
+        If you would like to request a feature, report a bug, or improve CrochetCraft, see
+        documentation and guidelines at the <a
+            class="anchor"
+            href="https://github.com/jazz-sim/crochet-craft">CrochetCraft GitHub repository</a
+        >.
+    </p>
+{/snippet}
+
+{#snippet learn()}
+    <p>
+        There are many tutorials online for learning - a large collection of resources can be found
+        in the <a class="anchor" href="https://www.reddit.com/r/crochet/wiki/quick_beginner_guide/"
+            >r/crochet Reddit community wiki</a
+        >.
+    </p>
+{/snippet}
+
+{#snippet experiments()}
+    While building the first version of CrochetCraft, the team made some <a
+        class="anchor"
+        href="https://crochetcraft.jtai.ca/experiments">demos and tools</a
+    >. These "mini-programs" are not production-ready.
+{/snippet}
+
 <div class={cBase}>
     <header class={cHeader}>
         <div class="flex items-center justify-center">
@@ -15,8 +51,32 @@
         </div>
     </header>
     <div id="aboutBody">
-        <div class="flex items-center justify-center">
-            <p>CrochetCraft is a Final Year Design Project (FYDP).</p>
+        <div class="flex flex-col items-center justify-center">
+            <p>
+                CrochetCraft is a <a
+                    class="anchor"
+                    href="https://uwaterloo.ca/capstone-design/project-abstracts/2025-capstone-design-projects"
+                    >Final Year Design Project (FYDP)</a
+                > from a team of University of Waterloo Software Engineering 2025 students.
+            </p>
+        </div>
+        <div class="mt-2 h-40 overflow-auto">
+            <IconCard icon="fi fi-br-question" title="How do I use CrochetCraft?" content={use} />
+            <IconCard
+                icon="fi fi-br-code-simple"
+                title="How can I contribute to CrochetCraft?"
+                content={contribute}
+            />
+            <IconCard
+                icon="fi fi-br-book-alt"
+                title="Where can I learn to crochet?"
+                content={learn}
+            />
+            <IconCard
+                icon="fi fi-br-wrench-simple"
+                title="I heard there's experiments..."
+                content={experiments}
+            />
         </div>
     </div>
     <footer class={parent.regionFooter}>
