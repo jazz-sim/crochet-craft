96b60b2eab1db2da64f9a83356ed315a8a03cf79
simcard0000 <simran.thind@outlook.com>
2025-03-08 22:19:38 -0500

    add example selector

--- crochetcraft/src/components/main-ui/Editor.svelte
+++ crochetcraft/src/components/main-ui/Editor.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
     import Panel, { type PanelPosition } from '$components/option-panel/Panel.svelte';
     import State from '$lib/state.svelte';
+    import { ListBox, ListBoxItem, popup, type PopupSettings } from '@skeletonlabs/skeleton';
     import { parse, link, place, elaborate, gdPlace } from 'crochet-stitcher';
     import type { LinkedStitch, ParsedInstruction, Pattern } from 'crochet-stitcher/types';
     import { Group } from 'three';
 
     let { position }: { position: PanelPosition } = $props();
 
+    // For the individual stitch buttons:
     interface AddStitchButtonData {
         name: string;
         hovertext: string;
@@ -14,16 +16,23 @@
     }
     const ADD_STITCH_BUTTONS: AddStitchButtonData[] = [
         { name: 'ch', hovertext: 'chain', disabled: false },
-        { name: 'slst', hovertext: 'slip stitch', disabled: true },
+        { name: 'slst', hovertext: 'slip stitch', disabled: false },
         { name: 'sc', hovertext: 'single crochet', disabled: false },
-        { name: 'dc', hovertext: 'double crochet', disabled: true },
-        { name: 'tr', hovertext: 'treble crochet', disabled: true },
-        { name: 'hdc', hovertext: 'half double crochet', disabled: true },
-        { name: 'inc', hovertext: 'increase', disabled: true },
-        { name: 'dec', hovertext: 'decrease', disabled: true },
+        { name: 'dc', hovertext: 'double crochet', disabled: false },
+        { name: 'tr', hovertext: 'treble crochet', disabled: false },
+        { name: 'hdc', hovertext: 'half double crochet', disabled: false },
+        { name: 'inc', hovertext: 'increase', disabled: false },
+        { name: 'dec', hovertext: 'decrease', disabled: false },
     ];
     let errorMessage: string | null = $state(null);
 
+    // For the example pattern combobox:
+    const popupComboboxExamplePatterns: PopupSettings = {
+        event: 'click',
+        target: 'popupCombobox',
+        placement: 'bottom',
+        closeQuery: '.listbox-item',
+    };
     const examplePatterns: { [key: string]: string } = {
         'default chain': 'ch 10',
         '3x3 square': 'ch 4, turn, sc 3, ch1, turn, sc3',
@@ -33,7 +42,7 @@
         '30 ring': 'ch 30, sc 1',
     };
     const examplePatternNames = Object.keys(examplePatterns);
-    let selectedExampleName = 'default chain';
+    let selectedExampleName = $state('default chain');
 
     let placerMaxIterations = 50;
     let mainGroup: Group = new Group();
@@ -43,6 +52,9 @@
     });
 
     function runPipeline() {
+        if (Object.values(examplePatterns).find((pat) => State.pattern === pat) == undefined) {
+            selectedExampleName = 'Select an example...';
+        }
         let parseResult: Pattern<ParsedInstruction>;
         // First, attempt to parse the input pattern:
         try {
@@ -73,6 +85,12 @@
         State.pattern += stitch;
         runPipeline();
     }
+
+    function exampleUpdatePattern() {
+        if (selectedExampleName != 'Select an example...') {
+            State.pattern = examplePatterns[selectedExampleName];
+        }
+    }
 </script>
 
 <Panel title="Pattern" {position}>
@@ -95,6 +113,7 @@
             class="whitespace-break-spaces">{errorMessage ?? '✅ Valid pattern.'}</pre>
     {/if}
 
+    <!-- Add stitch buttons -->
     <div class="flex flex-wrap gap-1">
         {#each ADD_STITCH_BUTTONS as { name, hovertext, disabled }}
             <button
@@ -109,6 +128,19 @@
         {/each}
     </div>
 
+    <!-- Example patterns combobox -->
+    <select
+        class="select rounded-lg"
+        bind:value={selectedExampleName}
+        onchange={exampleUpdatePattern}
+    >
+        <option disabled selected value="Select an example...">Select an example...</option>
+        {#each examplePatternNames as exPattern}
+            <option value={exPattern}>{exPattern}</option>
+        {/each}
+    </select>
+
+    <!-- Next Stitch Colour picker -->
     <label class="label">
         <span>Next Stitch Colour</span>
         <input
