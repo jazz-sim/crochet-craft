0a1fbfe2b59214dbc91e5bdeb2dfa25f76525e12
Dhananjay Patki <dpatki@uwaterloo.ca>
2025-03-10 17:34:45 -0400

    add double crochet

--- crochet-stitcher/src/models.ts
+++ crochet-stitcher/src/models.ts
@@ -4,6 +4,7 @@ import single from './models/single.json';
 import magicCircle from './models/magic-circle.json';
 import halfDoubleCrochet from './models/half-double-crochet.json';
 import trebleCrochet from './models/treble-crochet.json';
+import doubleCrochet from './models/double-crochet.json';
 
 interface StitchModel {
     curveType: 'bezier';
@@ -17,6 +18,7 @@ const StitchModel = {
     MAGIC_CIRCLE: magicCircle as StitchModel,
     HALF_DOUBLE_CROCHET: halfDoubleCrochet as StitchModel,
     TREBLE_CROCHET: trebleCrochet as StitchModel,
+    DOUBLE_CROCHET: doubleCrochet as StitchModel,
 };
 
 export default StitchModel;
