13bccb972bf494d452689b6ba679d9fdfb378152
simcard0000 <simran.thind@outlook.com>
2025-03-12 01:23:26 -0400

    Add to error page

--- crochetcraft/src/routes/+error.svelte
+++ crochetcraft/src/routes/+error.svelte
@@ -1,3 +1,22 @@
-<div class="flex h-full w-full flex-row items-center justify-center">
-    <p>Uh-oh! There's no page over here. Return home?</p>
+<script lang="ts">
+    import { goto } from '$app/navigation';
+    import { LightSwitch } from '@skeletonlabs/skeleton';
+</script>
+
+<div class="flex h-full flex-col">
+    <div class="flex w-full flex-row flex-wrap items-center gap-3 px-4 py-3">
+        <!-- Left side -->
+        <button class="btn-icon !bg-transparent" onclick={() => goto('/')}>
+            <img src="/cc-logo-new-2-purple.png" width="50" height="50" alt="CrochetCraft logo." />
+        </button>
+        <div class="flex-1"></div>
+        <!-- Right side -->
+        <LightSwitch rounded="rounded-lg" />
+    </div>
+    <div class="flex h-full flex-col items-center justify-center">
+        <img class="p-5" src="/cc-logo-new-1-purple.png" width="500px" alt="CrochetCraft logo." />
+        <h3 class="h3">
+            Uh-oh! There's no page here. <a class="anchor" href="/"><b>Return home?</b></a>
+        </h3>
+    </div>
 </div>
--- crochetcraft/src/routes/experiments/+layout.svelte
+++ crochetcraft/src/routes/experiments/+layout.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
-    import { AppBar, LightSwitch } from '@skeletonlabs/skeleton';
+    import { LightSwitch } from '@skeletonlabs/skeleton';
+    import { goto } from '$app/navigation';
 </script>
 
 <div class="flex h-full flex-col">
     <div class="flex w-full flex-row flex-wrap items-center gap-3 px-4 py-3">
         <!-- Left side -->
-        <img src="/cc-logo-new-2-purple.png" width="50" height="50" alt="CrochetCraft logo." />
+        <button class="btn-icon !bg-transparent" onclick={() => goto('/')}>
+            <img src="/cc-logo-new-2-purple.png" width="50" height="50" alt="CrochetCraft logo." />
+        </button>
         <div class="flex-1"></div>
         <b>CrochetCraft - Experimental Pages - <a class="anchor" href="/">Return home?</a></b>
         <div class="flex-1"></div>
